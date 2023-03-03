bd768e177f8b8c0840a5756a2dfc3635b2d374ea
StormLight14 <100660727+StormLight14@users.noreply.github.com>
2023-03-03 11:30:19 -0700

    Update chesscom-rpc.py

--- chesscom/chesscom-rpc.py
+++ chesscom/chesscom-rpc.py
@@ -1,30 +1,47 @@
 from pypresence import Presence
-import time
-import requests
+import time, requests, datetime
 
 client_id = "1079473290462117920"
-RPC = Presence(client_id)  # Initialize the client class
-RPC.connect()  # Start the handshake loop
+RPC = Presence(client_id)
+RPC.connect()
 
 start = int(time.time())
+
+today = datetime.date.today()
+month = f'{today.month:02d}'
 
 rapid_rating = 9999
 blitz_rating = 9999
 bullet_rating = 9999
 
-while True:  # The presence will stay on as long as the program is running
-    for i in range(1000):
-        if i == 1:
-            response = requests.get(
-                "https://api.chess.com/pub/player/stormlightnoway/stats").json()
+username = "Stormlightnoway"
 
-            # response = json.load(response)
-            rapid_rating = response["chess_rapid"]["last"]["rating"]
-            blitz_rating = response["chess_blitz"]["last"]["rating"]
-            bullet_rating = response["chess_bullet"]["last"]["rating"]
+# do not make this too low, don't spam chess.com api.
+sleep_time = 20
+
+api_delay = sleep_time * (sleep_time * 2)
+
+while True:
+    for i in range(api_delay):
+        # add a pretty big delay to checking elo stats
+        if i == 0:
+            stats = requests.get(
+                f"https://api.chess.com/pub/player/{username}/stats"
+                ).json()
+            
+            
+            rapid_rating = stats["chess_rapid"]["last"]["rating"]
+            blitz_rating = stats["chess_blitz"]["last"]["rating"]
+            bullet_rating = stats["chess_bullet"]["last"]["rating"]
+
+        # get the player's current game(s), check more often than stats
+        current_playing = requests.get(
+                f"https://api.chess.com/pub/player/{username}/games/{today.year}/{month}"
+            ).json()
+        print(len(current_playing))
 
         RPC.update(details=f"Rapid ELO: {rapid_rating}", start=start, large_image="large", buttons=[
-            {"label": "My Lichess Profile", "url": "https://lichess.org/@/Stormlightnoway"}, {"label": "My Chess.com Profile", "url": "https://www.chess.com/member/stormlightnoway"}])  # Set the presence
+            {"label": "My Lichess Profile", "url": f"https://lichess.org/@/{username}"}, {"label": "My Chess.com Profile", "url": f"https://www.chess.com/member/{username}"}])  # Set the presence
 
         start = start
-        time.sleep(1)
+        time.sleep(sleep_time)
